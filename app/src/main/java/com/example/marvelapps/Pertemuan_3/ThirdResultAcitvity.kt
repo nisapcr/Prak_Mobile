65ae17f393ca7ff95f9dbbdaa85b66bfee77820f
nisa_imut <harnisa24si@mnahasiswa.pcr.ac.id>
2026-04-01 10:24:25 +0700

    penerapan atribut dasn binding

--- app/src/main/java/com/example/marvelapps/Pertemuan_3/ThirdResultAcitvity.kt
+++ app/src/main/java/com/example/marvelapps/Pertemuan_3/ThirdResultAcitvity.kt
@@ -1,38 +1,21 @@
 package com.example.marvelapps.Pertemuan_3
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.marvelapps.R
-import com.example.marvelapps.databinding.ActivityThirdBinding
-import kotlin.jvm.java
 
-class ThirdActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityThirdBinding
+class ThirdResultAcitvity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityThirdBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        setContentView(R.layout.activity_third_result_acitvity)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        binding.btnKirim.setOnClickListener {
-            val isi = binding.inputNoTujuan.text
-
-            Toast.makeText(this, isi, Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, ThirdResultAcitvity::class.java)
-            startActivity(intent)
         }
     }
 }
