2fb0d9a051399d90258a6bdc2850616202d41689
nisa_imut <harnisa24si@mnahasiswa.pcr.ac.id>
2026-04-01 08:36:02 +0700

    penerapan atribut dasn binding

---
+++ app/src/main/java/com/example/marvelapps/Pertemuan_3/ThirdActivity.kt
@@ -0,0 +1,34 @@
+package com.example.marvelapps.Pertemuan_3
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.marvelapps.R
+import com.example.marvelapps.databinding.ActivityThirdBinding
+
+class ThirdActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityThirdBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.btnKirim.setOnClickListener {
+            val isi = binding.inputNoTujuan.text
+
+            Toast.makeText(this, isi, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
