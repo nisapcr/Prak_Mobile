8b0ae80721321f5adc2914c473db35eb1040ab6a
nisa_imut <harnisa24si@mnahasiswa.pcr.ac.id>
2026-04-08 10:06:11 +0700

    penerapan atribut dasn binding

---
+++ app/src/main/java/com/example/marvelapps/Pertemuan_4/FourthActivity.kt
@@ -0,0 +1,70 @@
+package com.example.marvelapps.Pertemuan_4
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.marvelapps.MainActivity
+import com.example.marvelapps.R
+import com.example.marvelapps.databinding.ActivityFourthBinding
+import com.example.marvelapps.databinding.ActivityMainBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
+
+class FourthActivity : AppCompatActivity() {
+        private lateinit var binding: ActivityFourthBinding
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            Log.e("onCreate", "FourthActivity dibuat pertama kali")
+            enableEdgeToEdge()
+            binding = ActivityFourthBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
+            val name = intent.getStringExtra("nama")
+            val from = intent.getStringExtra("asal")
+            val age = intent.getIntExtra("usia",0)
+            Log.e("Data Intent","Nama: $name , Usia: $age, Asal: $from")
+
+            binding.btnBack.setOnClickListener {
+                finish()
+            }
+            binding.btnShowSnackbar.setOnClickListener {
+                Snackbar.make(binding.root, "Ini adalah Snackbar", Snackbar.LENGTH_SHORT)
+                    .setAction("Tutup"){
+                        Log.e("Info Snackbar","Snackbar ditutup")
+                    }
+                    .show()
+            }
+            binding.btnShowAlertDialog.setOnClickListener {
+                MaterialAlertDialogBuilder(this)
+                    .setTitle("Konfirmasi")
+                    .setMessage("Apakah Anda yakin ingin melanjutkan?")
+                    .setPositiveButton("Ya") { dialog, _ ->
+                        dialog.dismiss()
+                        Log.e("Info Dialog","Anda memilih Ya!")
+                    }
+                    .setNegativeButton("Batal") { dialog, _ ->
+                        dialog.dismiss()
+                        Log.e("Info Dialog","Anda memilih Tidak!")
+                    }
+                    .show()
+            }
+        }
+        override fun onStart() {
+            super.onStart()
+            Log.e("onStart", "onStart: {nama_activity} terlihat di layar")
+        }
+
+        override fun onDestroy() {
+            super.onDestroy()
+            Log.e("onDestroy", "{nama_activity} dihapus dari stack")
+        }
+    }
+
