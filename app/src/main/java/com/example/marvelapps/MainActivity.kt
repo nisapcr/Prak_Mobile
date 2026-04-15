68e7f1bd2eed089b06707dd6f22650013d5e65c7
nisapcr <harnisa24@mahasiswa.pcr.ac.id>
2026-04-15 12:02:44 +0700

    Belajar mandiri

--- app/src/main/java/com/example/marvelapps/MainActivity.kt
+++ app/src/main/java/com/example/marvelapps/MainActivity.kt
@@ -2,40 +2,47 @@ package com.example.marvelapps
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.marvelapps.Pertemuan_3.ThirdResultAcitvity
 import com.example.marvelapps.Pertemuan_4.FourthActivity
+import com.example.marvelapps.Pertemuan_5.FifthActivity
 import com.example.marvelapps.databinding.ActivityMainBinding
-import com.example.marvelapps.databinding.ActivityThirdBinding
 
+class MainActivity : AppCompatActivity() {
 
-    class MainActivity : AppCompatActivity() {
-        private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            enableEdgeToEdge()
-            binding = ActivityMainBinding.inflate(layoutInflater)
-            setContentView(binding.root)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-                insets
-            }
-            binding.btnToFourth.setOnClickListener {
-                val intent = Intent(this, FourthActivity::class.java)
-                intent.putExtra("nama", "Politeknik Caltex Riau")
-                intent.putExtra("asal", "Rumbai")
-                intent.putExtra("usia", 25)
+        enableEdgeToEdge()
 
-                startActivity(intent)
+        // 🔥 INIT BINDING
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        // 🔥 HANDLE SAFE AREA
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
-            }
+        // 🔵 KE PERTEMUAN 4
+        binding.btnToFourth.setOnClickListener {
+            val intent = Intent(this, FourthActivity::class.java)
+            intent.putExtra("nama", "Politeknik Caltex Riau")
+            intent.putExtra("asal", "Rumbai")
+            intent.putExtra("usia", 25)
+            startActivity(intent)
+        }
+
+        // 🔵 KE PERTEMUAN 5
+        binding.btnToFifth.setOnClickListener {
+            val intent = Intent(this, FifthActivity::class.java)
+            startActivity(intent)
         }
     }
+}
---
+++ app/src/main/java/com/example/marvelapps/Pertemuan_5/FifthActivity.kt
@@ -0,0 +1,53 @@
+package com.example.marvelapps.Pertemuan_5
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.marvelapps.R
+import com.google.android.material.appbar.MaterialToolbar
+
+class FifthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fifth)
+
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val btn = findViewById<Button>(R.id.btnWeb)
+
+        btn.setOnClickListener {
+            startActivity(Intent(this, WebViewActivity::class.java))
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+
+            R.id.menu_about -> {
+                Toast.makeText(this, "Halaman About", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.menu_dark -> {
+                item.isChecked = !item.isChecked
+                Toast.makeText(this, "Dark Mode: ${item.isChecked}", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.menu_notif -> {
+                item.isChecked = !item.isChecked
+                Toast.makeText(this, "Notifikasi: ${item.isChecked}", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return true
+    }
+}
